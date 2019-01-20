7119d8f48793007be7c441c1d809bde19de82932
Caleb Hester <naclcaleb@gmail.com>
2019-01-19 16:30:55 -0800

    Fix Alpha - Fixes #8 - rebuild pages

--- brushes.js
+++ brushes.js
@@ -8,42 +8,47 @@ var DEFAULT_BRUSH = new Brush(function(ctx, e){
                 mouseX = e.clientX,
                 mouseY = e.clientY;
             
-            var d = dist(pmouseX, pmouseY, mouseX, mouseY);
-            var angle = angleBetween(pmouseX, pmouseY, mouseX, mouseY);
-            var lastX = pmouseX;
-            var lastY = pmouseY;
+    var d = dist(pmouseX, pmouseY, mouseX, mouseY);
+    var angle = angleBetween(pmouseX, pmouseY, mouseX, mouseY);
+    var lastX = pmouseX;
+    var lastY = pmouseY;
+
+    this.currentStroke.addAction({
+        func: ctx.setGlobalAlpha, 
+        params: [1]
+    });
             
-            for (var i = 0;i<d;i+=BRUSH_SIZE/8){
+    for (var i = 0;i<d;i+=BRUSH_SIZE/8){
                 
-                var x = lastX + (i * Math.sin(angle));
-                var y = lastY + (i * Math.cos(angle));
+        var x = lastX + (i * Math.sin(angle));
+        var y = lastY + (i * Math.cos(angle));
 
-                let radgrad = ctx.createRadialGradient(
-                    x, y, 0, x, y, BRUSH_SIZE/2);
+        let radgrad = ctx.createRadialGradient(x, y, 0, x, y, BRUSH_SIZE/2);
                 
                 
-                radgrad.addColorStop(0, colorToString(COLOR));
+        radgrad.addColorStop(0, colorToString(COLOR));
 
-                var pastColorAlpha = COLOR.alpha + 1;
+        var pastColorAlpha = COLOR.alpha + 1;
                 
-                COLOR.alpha /= 2;
-                radgrad.addColorStop(0.5, colorToString(COLOR));
-                COLOR.alpha = 0;
-                radgrad.addColorStop(1, colorToString(COLOR));
-                COLOR.alpha = pastColorAlpha - 1;
-                var action = {
-                    func: ctx.setFillStyle,
-                    params: [radgrad]
-                };
-
-                var action2 = {
-                    func: ctx.fillRect,
-                    params: [x-BRUSH_SIZE/2, y-BRUSH_SIZE/2, BRUSH_SIZE,BRUSH_SIZE]
-                };
-                this.currentStroke.addAction(action);
-                this.currentStroke.addAction(action2);
-
+        COLOR.alpha /= 2;
+        radgrad.addColorStop(0.5, colorToString(COLOR));
+        COLOR.alpha = 0;
+        radgrad.addColorStop(1, colorToString(COLOR));
+        COLOR.alpha = pastColorAlpha - 1;
+        var actions = [
+            {
+                func: ctx.setFillStyle,
+                params: [radgrad]
+            },
+            {
+                func: ctx.fillRect, 
+                params: [x-BRUSH_SIZE/2, y-BRUSH_SIZE/2, BRUSH_SIZE,BRUSH_SIZE]
             }
+        ];
+
+        this.currentStroke.addActions(actions);
+
+    }
 
                
 });
@@ -201,51 +206,56 @@ var SOLID = new Brush(function(ctx, e){
                 mouseX = e.clientX,
                 mouseY = e.clientY;
             
-            var d = dist(pmouseX, pmouseY, mouseX, mouseY);
-            var angle = angleBetween(pmouseX, pmouseY, mouseX, mouseY);
-            var lastX = pmouseX;
-            var lastY = pmouseY;
+    var d = dist(pmouseX, pmouseY, mouseX, mouseY);
+    var angle = angleBetween(pmouseX, pmouseY, mouseX, mouseY);
+    var lastX = pmouseX;
+    var lastY = pmouseY;
 
-            const setFillStyle = [
-                {
-                    func: ctx.setFillStyle,
-                    params: [colorToString(COLOR)]
-                },
-                {
-                    func: ctx.setGlobalAlpha,
-                    params: [COLOR.alpha]
-                }
-            ];
+    var pastAlpha = ctx.globalAlpha + 1 - 1;
 
-            this.currentStroke.addActions(setFillStyle);
+    const setFillStyle = [
+        {
+            func: ctx.setFillStyle,
+            params: [colorToString(COLOR)]
+        },
+        {
+            func: ctx.setGlobalAlpha,
+            params: [COLOR.alpha]
+        }
+    ];
+
+    this.currentStroke.addActions(setFillStyle);
             
-            for (var i = 0;i<d;i+=BRUSH_SIZE/4){
+    for (var i = 0;i<d;i+=BRUSH_SIZE/4){
                 
-                var x = lastX + (i * Math.sin(angle));
-                var y = lastY + (i * Math.cos(angle));
+        var x = lastX + (i * Math.sin(angle));
+        var y = lastY + (i * Math.cos(angle));
 
-                //Draw an ellipse
+        //Draw an ellipse
                 
-                 let actions = [
-                     {
-                        func: ctx.beginPath,
-                        params: []
-                     },
-                     {
-                        func: ctx.arc, 
-                        params: [x, y, BRUSH_SIZE, 0, 2 * Math.PI]
-                    },
-                    {
-                        func: ctx.fill,
-                        params: []
-                    }
-                ];
-
-
-                this.currentStroke.addActions(actions);
+        let actions = [
+            {
+                func: ctx.beginPath,
+                params: []
+            },
+            {
+                func: ctx.arc, 
+                params: [x, y, BRUSH_SIZE, 0, 2 * Math.PI]
+            },
+            {
+                func: ctx.fill,
+                params: []
             }
+        ];
 
-               
+
+        this.currentStroke.addActions(actions);
+    }
+
+    this.currentStroke.addAction({
+        func: ctx.setGlobalAlpha,
+        params: [pastAlpha]
+    });
 });
 
 var callig_instance_img = new Image();
