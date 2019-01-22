7230a85413b77b80fbbbfd5eeca10a868750e6e3
OOPS! Studio! <46879790+OOPS-Studio@users.noreply.github.com>
2019-01-21 16:42:58 -0800

    Added redoing.
    This modification was done by OOPS! Studio!

--- Layer.js
+++ Layer.js
@@ -171,6 +171,17 @@ class Layer {
         //Recreate the render
         this.createRender();
     }
+	
+	redo() {
+        //Get the last undone stroke
+        var stroke = this.undone.shift();
+
+        //Add it back to our drawing
+        this.stroke.unshift(stroke);
+
+        //Rerender the drawing
+        this.updateRender();
+    }
 
     draw() {
         //Clear the canvas (Possibly unnecessary)
